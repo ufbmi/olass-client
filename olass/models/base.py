88569b518fdf8221e7fcef32616d552c5fe8d1d8
Andrei Sura <sura.andrei@gmail.com>
2016-06-09 19:09:50 -0400

    Add first model olass/models/patient.py extending the `declarativebase` in olass/models/base.py

---
+++ olass/models/base.py
@@ -0,0 +1,45 @@
+"""
+Goal: implement the base class for models
+"""
+import sqlalchemy as db  # NOQA
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext import declarative
+
+# import sqlalchemy.sql as sasql
+# import sqlalchemy.ext.declarative as sad
+# from sqlalchemy.ext.hybrid import hybrid_property
+
+maker = sessionmaker(autoflush=True, autocommit=False)
+DBSession = scoped_session(maker)
+
+
+class ReprBase():
+
+    """
+    Provides a nicer representation when a class instance is printed.
+    """
+
+    def __repr__(self):
+        """
+        Note: does not print attributes with "_" as prefix
+        """
+        return "%s(%s)" % (
+            (self.__class__.__name__),
+            ', '.join(["%s=%r" % (key, getattr(self, key))
+                       for key in sorted(self.__dict__.keys())
+                       if not key.startswith('_')]))
+
+DeclarativeBase = declarative.declarative_base(cls=ReprBase)
+metadata = DeclarativeBase.metadata
+
+
+def init(engine):
+    """
+    Call this method before using any of the ORM classes.
+
+    :seealso: :meth:olass_client.get_db_session()
+    """
+    DBSession.configure(bind=engine)
+
+# remember to define __ALL__ in each entity we define
+from olass.models.patient import Patient  # NOQA
---
+++ olass/models/patient.py
@@ -0,0 +1,53 @@
+"""
+Goal: Implement ORM for `patient` table
+"""
+import sqlalchemy as db
+from olass.models.base import DeclarativeBase
+# from crud_mixin import CRUDMixin
+
+__all__ = ['Patient']
+
+"""
++---------------------+---------------------+------+-----+---------+
+| Field               | Type                | Null | Key | Default |
++---------------------+---------------------+------+-----+---------+
+| pat_id              | bigint(20) unsigned | NO   | PRI | NULL    |
+| pat_mrn             | varchar(255)        | NO   | UNI | NULL    |
+| linkage_uuid        | binary(16)          | YES  | MUL | NULL    |
+| linkage_added_at    | datetime            | YES  | MUL | NULL    |
+| gender              | char(2)             | YES  | MUL | NULL    |
+| race                | char(2)             | YES  | MUL | NULL    |
+| hispanic_ethnicity  | char(2)             | YES  | MUL | NULL    |
+| pat_birth_date      | date                | NO   |     | NULL    |
+| pat_first_name      | varchar(255)        | NO   |     | NULL    |
+| pat_last_name       | varchar(255)        | NO   |     | NULL    |
+| pat_middle_name     | varchar(255)        | YES  |     | NULL    |
+| pat_phone           | varchar(255)        | YES  |     | NULL    |
+| pat_address_line    | varchar(255)        | YES  |     | NULL    |
+| pat_address_city    | varchar(255)        | NO   |     | NULL    |
+| pat_address_state   | char(2)             | YES  |     | NULL    |
+| pat_address_zip     | char(5)             | NO   |     | NULL    |
+| pat_address_country | char(2)             | YES  |     | NULL    |
++---------------------+---------------------+------+-----+---------+
+"""
+
+
+# class Patient(DeclarativeBase, CRUDMixin):
+class Patient(DeclarativeBase):
+
+    """
+    Store data about the patient
+    """
+    __tablename__ = 'patient'
+    id = db.Column('pat_id', db.Integer, primary_key=True)
+    pat_mrn = db.Column('pat_mrn', db.Text, nullable=False)
+
+    # These two columns are populated when we receive json from OLASS server
+    linkage_uuid = db.Column('linkage_uuid', db.Binary)
+    linkage_addded_at = db.Column('linkage_added_at', db.DateTime)
+
+    pat_birth_date = db.Column('pat_birth_date', db.DateTime, nullable=False)
+    pat_first_name = db.Column('pat_first_name', db.Text, nullable=False)
+    pat_last_name = db.Column('pat_last_name', db.Text, nullable=False)
+    pat_address_city = db.Column('pat_address_city', db.Text, nullable=False)
+    pat_address_zip = db.Column('pat_address_zip', db.Text, nullable=False)
