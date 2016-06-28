d03ca4be113099cecd7dc7a94c999dee696f8ed2
Andrei Sura <sura.andrei@gmail.com>
2016-06-28 12:08:39 -0400

    Rename and cleanup config.py -> config_examle.py

--- config_examle.py
+++ config_examle.py
@@ -5,22 +5,21 @@ Goal: store application settings
 # Set to False if the a target server has a self-signed certificate
 VERIFY_SSL_CERT = False
 
-#
 CLIENT_ID = 'client_1'
 CLIENT_SECRET = 'secret_1'
 
-# TOKEN_BASE_URL = 'https://localhost:5000/oauth/token'
-# SECURE_DATA_URL = 'https://localhost:5000/api/me'
-
 # Application endpoints
-TOKEN_URL = 'https://ahc-olass19b.ahc.ufl.edu/oauth/token'
-SAVE_URL = 'https://ahc-olass19b.ahc.ufl.edu/api/save'
-
-SEND_TO_DB = True
+TOKEN_URL = 'https://localhost:5000/oauth/token'
+SAVE_URL = 'https://localhost:5000/api/save'
 
 DB_HOST = 'localhost'
 DB_PORT = 3306
-
 DB_NAME = 'olassc'
 DB_USER = 'olassc'
 DB_PASS = 'insecurepassword'
+
+# ##############################################
+# Params used while running run_data_import.py #
+# ##############################################
+# @see utils:process_frame()
+SEND_TO_DB = True
