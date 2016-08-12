c3d7e72e69cfccf38061fdee5505e5289dd1e85c
Andrei Sura <sura.andrei@gmail.com>
2016-08-11 21:15:26 -0400

    make Travis happy about olass/version.py

--- olass/version.py
+++ olass/version.py
@@ -2,7 +2,12 @@
 Goal: provide access to the version of the installed package.
 """
 import pkg_resources
-__version__ = pkg_resources.require("olass")[0].version
+
+try:
+    __version__ = pkg_resources.require("olass")[0].version
+except Exception:
+    from setuptools_scm import get_version
+    __version__ = get_version()
 
 
 if __name__ == "__main__":
